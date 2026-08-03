68ab74161bbfd9e102e53570e75a6c112bb1d9b0
Geraldine <geca8469@colorado.edu>
2026-08-03 05:08:46 +0000

    Move landing page into Flask template

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
 import db
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_for=1,
+    x_proto=1,
+    x_host=1,
+    x_prefix=1
+)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
