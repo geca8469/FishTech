bb82caeabd9a20f5652ecff866639c2d3852a1bc
Mason Chansamone <mach3999@colorado.edu>
2026-08-05 04:05:19 +0000

    Revert "changed location for / route template location"
    This reverts commit 604c817b7c1be9834c7db6bea1b7c16e671142bd.

--- webapp/app.py
+++ webapp/app.py
@@ -15,7 +15,7 @@ app.wsgi_app = ProxyFix(
 ### WEB ROUTES
 @app.route("/")
 def home():
-    return render_template("templates/home.html")
+    return render_template("home.html")
 
 
 ### TESTING ROUTES
