d7646e4931a5ad53e3303846c458a4c321c0f171
Mason Chansamone <mach3999@colorado.edu>
2026-08-05 06:54:45 +0000

    add_user and show_table test routes

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
 from werkzeug.middleware.proxy_fix import ProxyFix
 import db
 
 app = Flask(__name__)
+app.secret_key = 'a secret key'
 
 app.wsgi_app = ProxyFix(
     app.wsgi_app,
@@ -20,12 +21,21 @@ def home():
 @app.route("/fish-collection")
 def fish_collection():
     return render_template("fish-collection.html")
+    
 
 
 ### TESTING ROUTES
 @app.route('/create_tables')
 def create_tables():
     return db.create_tables()
+
+@app.route('/show_table/<table_name>')
+def show_table(table_name):
+    return db.get_table(table_name)
+
+@app.route('/add_user')
+def add_user():
+    return db.add_user('name', 'pass')
 
 if __name__ == "__main__":
     app.run(
